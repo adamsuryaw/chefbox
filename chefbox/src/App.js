1f8373ed4b8441c2bdf99d481d1073cc7ec0f230
rizzie03 <destin.rizzie@gmail.com>
2021-11-28 23:46:10 +0700

    adding styling in home

--- chefbox/src/App.js
+++ chefbox/src/App.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import "./App.css";
+import Routers from "./routes/Routes";
 
 function App() {
   return (
     <>
-    <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-    </Router>
-      
+      <Routers />
+      <Footer />
     </>
   );
 }
---
+++ chefbox/src/page/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.body}>
+      <div className={styles.topSections}>
+          
+      </div>
+      <div className={styles.midSections}></div>
+      <div className={styles.botSections}></div>
+    </div>
+  );
+}
