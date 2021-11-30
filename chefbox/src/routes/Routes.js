0e332b0138f5f085d089ab2861fc1b18fcf3c436
rizzie03 <destin.rizzie@gmail.com>
2021-11-30 11:06:46 +0700

    adding some stuff

--- chefbox/src/routes/Routes.js
+++ chefbox/src/routes/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../page/home/Home";
+import Order from "../page/order/Order";
 
 export default function Routers() {
   return (
     <>
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/order' element={<Order />} />
       </Routes>
     </>
   );
