1181bd3876fbcce2066d84991ce43df7dbf62fb4
Adam Surya <surya.adam24@gmail.com>
2021-11-30 09:35:14 +0700

    feature/signup

--- chefbox/src/App.js
+++ chefbox/src/App.js
@@ -1,10 +1,11 @@
 import Register from "./Component/Register/Register";
+import Signup from "./Component/Signup/Signup";
 import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Signup />
     </div>
   );
 }
---
+++ chefbox/src/Component/Signup/Signup.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import './Signup.css'
+import bgCover from '../../Asset/Frame 4.svg'
+import logo from '../../Asset/Group 2.svg'
+import { Grid, Paper, Box, Button, Divider } from '@mui/material';      
+import { styled } from '@mui/material/styles';
+import { Formik, Form } from 'formik';
+import ButtonRegis from '../Button/ButtonRegis'
+import * as Yup from 'yup';
+import Auth from '../Auth/Auth';
+import AuthRegis from '../Auth/AuthRegis';
+import TextBarSignup from '../TextBar/TextBarSignup';
+import UploadImage from '../Upload/UploadImage';
+import ButtonSignup from '../Button/ButtonSignup';
+
+function Signup() {
+    const validate = Yup.object({
+        username: Yup.string()
+            .max(15, 'Must be 20 character or less')
+            .required('Required'),
+        email: Yup.string()
+            .email("Email is Invalid")
+            .required('Required')
+            .matches(
+                /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                "Please Input Your Email"
+            ),
+        password: Yup.string("Please Enter Your Password")
+            .required('Password must be required')
+            .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+                "Password Must be at least 8 Characters"
+            ),
+        confirmPassword: Yup.string()
+            .required('Re-enter your password')
+            .oneOf([Yup.ref("password"), null], "Password must match"),
+    })
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
+    return (
+        <div className="signup-page">
+            <div className="signup-background">
+                <img className="bg-image" src={bgCover} alt="background image" />
+                <Grid container>
+                    <Grid item xl={6}>
+                        <img className="logo-image" src={logo} alt="logo image" />
+                    </Grid>
+                    <Grid item xl={6}>
+                        <Box 
+                            sx={{
+                                backgroundColor: 'white',
+                                width: '100%',
+                                height: '911px',
+                                borderRadius: '80px 0 0 80px',
+                                marginTop: '60px',
+                                boxShadow: '0px 4px 24px 4px rgba(0, 0, 0, 0.15)'
+                            }}
+                        >
+                            <Formik
+                                initialValues = {{
+                                    username: '',
+                                    email: '',
+                                    passowrd: '',
+                                    confirmPassword: '',
+                                }}
+                                validationSchema={validate}
+                            >
+                                {formik => 
+                                    <div className="field-top">
+                                        <div className="name-top">
+                                            <h1>One more step!</h1>
+                                            <h6>Help us know you better</h6>
+                                        </div>
+                                        {/* {console.log(formik)} */}
+                                        <UploadImage />
+                                        <Form>
+                                            <TextBarSignup label="Phone Number" name="phoneNumber" type="text" />
+                                            <TextBarSignup label="Address" name="address" type="text" />
+                                            <TextBarSignup label="Location (City)" name="location" select="true" type="text"/>
+                                            <ButtonSignup />
+                                        </Form>
+                                    </div>
+                                }
+                            </Formik>
+                        </Box>
+                    </Grid>
+                </Grid>
+                
+            </div>
+        </div>
+        
+    )
+}
+
+export default Signup
