f3772fe5fef33606dbe5b070bdcf83dc96c4a0cb
rizzie03 <destin.rizzie@gmail.com>
2021-12-03 09:55:02 +0700

    fixing routes in develop branch

--- chefbox/src/routes/Routes.js
+++ chefbox/src/routes/Routes.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../page/home/Home";
 import Order from "../page/order/Order";
-import Register from "./Component/Register/Register";
-import Login from "./Pages/Login/Login";
+import Register from "../Component/Register/Register";
+import Login from "../Pages/Login/Login";
 
 export default function Routers() {
   return (
