5498fed789173693de9aaca6ae3a6e829fcb1524
rizzie03 <destin.rizzie@gmail.com>
2021-12-09 10:08:11 +0700

    adding some style in seller

--- src/page/dashboard/Seller.jsx
+++ src/page/dashboard/Seller.jsx
@@ -11,14 +11,14 @@ export default function Seller() {
       <div className={styles.seller}>
         <div className={styles.menuBarSeller}>
           <div className={styles.menuBar}>
-            <Link to="">My Recipe</Link>
-            <Link to="">My Order</Link>
+            <Link to=''>My Recipe</Link>
+            <Link to=''>My Order</Link>
           </div>
           <div className={styles.sellerBar}>
             <div className={styles.iconStore}>
-              <img src={sellerIcon} alt="" />
+              <img src={sellerIcon} alt='' />
             </div>
-            <Link to="">Seller Dashboard</Link>
+            <Link to=''>Seller Dashboard</Link>
           </div>
         </div>
         <div className={styles.menuSeller}>
@@ -29,13 +29,13 @@ export default function Seller() {
               <div className={styles.list}>
                 <button className={styles.buttonshipping}>
                   <div className={styles.iconbutton}>
-                    <img src={buttonShipping} alt="" />
+                    <img src={buttonShipping} alt='' />
                     <span>Prepare Shipping</span>
                   </div>
                 </button>
                 <button className={styles.buttoncompleted}>
                   <div className={styles.iconbutton}>
-                    <img src={buttonCompleted} alt="" />
+                    <img src={buttonCompleted} alt='' />
                     <span>Completed</span>
                   </div>
                 </button>
@@ -45,7 +45,7 @@ export default function Seller() {
           <div className={styles.menuCollections}>
             <div className={styles.menuContainer}>
               <div className={styles.menuImage}>
-                <img src={MenuImage} alt="" />
+                <img src={MenuImage} alt='' />
               </div>
               <div className={styles.menuDescriptions}>
                 <div className={styles.menuCollectionButton}>
@@ -61,7 +61,7 @@ export default function Seller() {
                   <p>1 package</p>
                 </div>
                 <div className={styles.descriptionDates}>
-                  <Link to="/details">see ingredient details</Link>
+                  <Link to='/details'>see ingredient details</Link>
                   <p>26 Jul 2021</p>
                 </div>
               </div>
@@ -70,7 +70,7 @@ export default function Seller() {
           <div className={styles.menuCollections}>
             <div className={styles.menuContainer}>
               <div className={styles.menuImage}>
-                <img src={MenuImage} alt="" />
+                <img src={MenuImage} alt='' />
               </div>
               <div className={styles.menuDescriptions}>
                 <div className={styles.menuCollectionButton}>
@@ -80,7 +80,7 @@ export default function Seller() {
                   <div className={styles.button1}>
                     <p>
                       Completed
-                      <img src={buttonCompleted} alt="" />
+                      <img src={buttonCompleted} alt='' />
                     </p>
                   </div>
                 </div>
@@ -89,7 +89,7 @@ export default function Seller() {
                   <p>1 package</p>
                 </div>
                 <div className={styles.descriptionDates}>
-                  <Link to="/details">see ingredient details</Link>
+                  <Link to='/details'>see ingredient details</Link>
                   <p>26 Jul 2021</p>
                 </div>
               </div>
@@ -98,7 +98,7 @@ export default function Seller() {
           <div className={styles.menuCollections}>
             <div className={styles.menuContainer}>
               <div className={styles.menuImage}>
-                <img src={MenuImage} alt="" />
+                <img src={MenuImage} alt='' />
               </div>
               <div className={styles.menuDescriptions}>
                 <div className={styles.menuCollectionButton}>
@@ -107,7 +107,7 @@ export default function Seller() {
                   </div>
                   <div className={styles.button2}>
                     <p>
-                      <img src={buttonShipping} alt="" />
+                      <img src={buttonShipping} alt='' />
                       Prepare Shipping
                     </p>
                   </div>
@@ -120,7 +120,94 @@ export default function Seller() {
                   <p>1 package</p>
                 </div>
                 <div className={styles.descriptionDates}>
-                  <Link to="/details">see ingredient details</Link>
+                  <Link to='/details'>see ingredient details</Link>
+                  <p>26 Jul 2021</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.menuCollections}>
+            <div className={styles.menuContainer}>
+              <div className={styles.menuImage}>
+                <img src={MenuImage} alt='' />
+              </div>
+              <div className={styles.menuDescriptions}>
+                <div className={styles.menuCollectionButton}>
+                  <div className={styles.title2}>
+                    <h4>Baked Oatmeal with Mixed Berries</h4>
+                  </div>
+                  <div className={styles.button1}>
+                    <p>
+                      Completed
+                      <img src={buttonCompleted} alt='' />
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.billDescription}>
+                  <h4>Rp.135.000</h4>
+                  <p>1 package</p>
+                </div>
+                <div className={styles.descriptionDates}>
+                  <Link to='/details'>see ingredient details</Link>
+                  <p>26 Jul 2021</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.menuCollections}>
+            <div className={styles.menuContainer}>
+              <div className={styles.menuImage}>
+                <img src={MenuImage} alt='' />
+              </div>
+              <div className={styles.menuDescriptions}>
+                <div className={styles.menuCollectionButton}>
+                  <div className={styles.title3}>
+                    <h4>Baked Oatmeal with Mixed Berries</h4>
+                  </div>
+                  <div className={styles.button2}>
+                    <p>
+                      <img src={buttonShipping} alt='' />
+                      Prepare Shipping
+                    </p>
+                  </div>
+                  <div className={styles.button}>
+                    <button>Verify Payment</button>
+                  </div>
+                </div>
+                <div className={styles.billDescription}>
+                  <h4>Rp.135.000</h4>
+                  <p>1 package</p>
+                </div>
+                <div className={styles.descriptionDates}>
+                  <Link to='/details'>see ingredient details</Link>
+                  <p>26 Jul 2021</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.menuCollections}>
+            <div className={styles.menuContainer}>
+              <div className={styles.menuImage}>
+                <img src={MenuImage} alt='' />
+              </div>
+              <div className={styles.menuDescriptions}>
+                <div className={styles.menuCollectionButton}>
+                  <div className={styles.title2}>
+                    <h4>Baked Oatmeal with Mixed Berries</h4>
+                  </div>
+                  <div className={styles.button1}>
+                    <p>
+                      Completed
+                      <img src={buttonCompleted} alt='' />
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.billDescription}>
+                  <h4>Rp.135.000</h4>
+                  <p>1 package</p>
+                </div>
+                <div className={styles.descriptionDates}>
+                  <Link to='/details'>see ingredient details</Link>
                   <p>26 Jul 2021</p>
                 </div>
               </div>
